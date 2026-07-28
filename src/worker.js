518499bf173824740216823a24f3855a6111da53
ore <oresama4@hotmail.com>
2026-07-28 00:11:07 -0600

    Initial commit (by create-cloudflare CLI)
    Details:
      C3 = create-cloudflare@2.70.14
      project name = blue-thunder-d51f
      package manager = npm@11.17.0
      wrangler = wrangler@4.114.0
      git = 2.50.1 (Apple Git-155)

---
+++ src/worker.js
@@ -0,0 +1,23 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env, ctx) {
+    if (request.method !== "POST") {
+      return new Response("Método no permitido", { status: 405 });
+    }
+
+    const data = await request.json();
+
+    console.log(data)
+
+    return new Response(data.name);
+  }
+};
